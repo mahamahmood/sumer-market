05ae4baf698d1b7693f4990ccc353686f4c620c1
Maha Mahmood <>
2020-08-20 20:30:09 -0400

    added dotenv to keep secrets out of server.js. Wrote some basic code to set up mongoose, methodOverride, and middleware

--- server.js
+++ server.js
@@ -1,7 +1,28 @@
+require('dotenv').config()
+
 // Dependencies
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 
+// Constants
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+app.use(methodOverride('_method'));
+const mongoURI = process.env.MONGO_URI;
+// Mongoose Connection
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.once('open', () => {
+    console.log('Connected to mongo!')
+});
+
+// Controllers
 
 // Routes
 app.get('/products', (req, res) => {
@@ -9,6 +30,6 @@ app.get('/products', (req, res) => {
 })
 
 // PORT Listen
-app.listen(3000, () => {
-    console.log("I'm listening")
-})
+app.listen(PORT, () => {
+    console.log("I'm listening on port: " + PORT)
+});
