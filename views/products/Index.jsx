e1ea07a52f2909a1ff7fa69ff0842d9e2a33ac45
Maha Mahmood <>
2020-08-24 17:37:30 -0400

    styled products index page

--- views/products/Index.jsx
+++ views/products/Index.jsx
@@ -8,28 +8,40 @@ class Index extends React.Component {
         return (
             <Default>
                 <div>
-                    <h1>Index Page / All Products</h1>
-                    <a href="/products/new">Create a New Product</a>
-                    <ul>
-                        {
-                            products.map((product, i) => {
-                                return (
-                                    <div>
-                                        {/* a name, img, and price of each product will be shown on the index/products page */}
-                                        <li>Name: {product.name}</li>
-                                        <a href={`/products/${product._id}`}><img src={product.img} /></a>
-                                        <li>Price: ${product.price}</li>
-
-                                        {/* override post method to delete the product of this uniq id */}
-                                        <form action={`/products/${product._id}?_method=DELETE`} method='POST'>
-                                            <input type='submit' value='Delete Product' />
-                                        </form>
-                                        <a href={`/products/${product._id}/edit`}>Edit Product</a>
-                                    </div>
-                                );
-                            })
-                        }
-                    </ul>
+                    <header>
+                        <img class="sumer-banner img-fluid" src="https://i.imgur.com/9siX5Xj.png" />
+                        <h1 class='sumer-mine-h1'>Sumer Products</h1>
+                        <div class="sumer-p-btn-create">
+                            <p>Sweet beans affogato, sweet, instant aroma sit in flavour. Chicory, coffee pumpkin spice latte espresso, frappuccino single origin crema trifecta percolator coffee strong. Americano ut cappuccino as, qui, crema cultivar cappuccino affogato black robust.</p>
+                            <a class="btn btn-outline-dark" role="button" href="/products/new">Create a New Product Page</a>
+                        </div>
+                    </header>
+                    <div class="container">
+                        <ul class="row row-cols-1 row-cols-md-2">
+                            {
+                                products.map((product, i) => {
+                                    return (
+                                        <div class="col mb-4">
+                                            {/* a name, img, and price of each product will be shown on the index/products page */}
+                                            <div class="card card mb-1 sumer-card">
+                                                <li class="card-header card-title">{product.name}</li>
+                                                <div class="card-body">
+                                                    <a href={`/products/${product._id}`}><img class="img-fluid card-img" src={product.img} /></a>
+                                                    <li class="card-title">Price: ${product.price}</li>
+                                                    <a class="btn btn-outline-dark btn-product" role="button" href={`/products/${product._id}`}>View</a>
+                                                    <a class="btn btn-outline-dark btn-product" role="button" href={`/products/${product._id}/edit`}>Edit</a>
+                                                    {/* override post method to delete the product of this uniq id */}
+                                                    <form action={`/products/${product._id}?_method=DELETE`} method='POST'>
+                                                        <input class="btn btn-outline-dark btn-product" type='submit' value='Delete' />
+                                                    </form>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    );
+                                })
+                            }
+                        </ul>
+                    </div>
                 </div>
             </Default>
         );
--- views/recipes/Index.jsx
+++ views/recipes/Index.jsx
@@ -8,14 +8,12 @@ class Index extends React.Component {
             <Default>
                 <div>
                     <header>
-                        <img class="recipe-banner img-fluid" src="https://i.imgur.com/9siX5Xj.png" />
-                        <div>
-                            <h1 class='recipe-mine-h1'>Sumer Recipes <small><i>Shared by Our Community</i></small></h1>
-                            <div class="recipe-p-create">
+                        <img class="sumer-banner img-fluid" src="https://i.imgur.com/9siX5Xj.png" />
+                            <h1 class='sumer-mine-h1'>Sumer Recipes <small><i>Shared by Our Community</i></small></h1>
+                            <div class="sumer-p-btn-create">
                                 <p>Our customers not only shop at Sumer Market but also they come here to share their best tips and tricks of making the best middel eastern dishes. Here you will be able to view all of the recipes that have been shared and <a href="/recipes/new">create your own recipe page.</a></p>
                                 <a class="btn btn-outline-dark" role="button" href="/recipes/new">Create a New Recipe Page</a>
                             </div>
-                        </div>
                     </header>
                     <div class="container">
                         <ul class="row row-cols-1 row-cols-md-2">
@@ -23,7 +21,7 @@ class Index extends React.Component {
                                 recipes.map((recipe, i) => {
                                     return (
                                         <div class="col mb-4">
-                                            <div class="col card mb-1 recipe-card">
+                                            <div class="col card mb-1 sumer-card">
                                                 <a href={`/recipes/${recipe._id}`}><img class="img-fluid card-img-top" src={recipe.img} /></a>
                                                 <div class="card-body">
                                                     <li class="card-title">{recipe.title}</li>
