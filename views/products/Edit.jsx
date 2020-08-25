ca06990c216027b89f92a703dc900b2161eac01b
Maha Mahmood <>
2020-08-24 23:13:55 -0400

    styled product edit page

--- views/products/Edit.jsx
+++ views/products/Edit.jsx
@@ -6,17 +6,27 @@ class Edit extends React.Component {
         const { _id, name, img, description, qty, price } = this.props.product;
         return (
             <Default>
-                <div>
-                    <h1>Product Edit Page</h1>
-                    {/* use method override to change the method to PUT instead of POST */}
-                    <form action={`/products/${_id}?_method=PUT`} method='POST'>
-                        Name: <input type='text' name='name' defaultValue={name} /><br />
-                    Image URL: <input type='text' name='img' defaultValue={img} /><br />
-                    Description: <input type='text' name='description' defaultValue={description} /><br />
-                    Quantity: <input type='number' name='qty' defaultValue={qty} /><br />
-                    Price: <input type='number' name='price' defaultValue={price} /><br />
-                        <input type='submit' name='' value='Submit Changes' />
-                    </form>
+                <div class="form-container">
+                    <div class="form">
+                        <h1 class="h1-form">Product Edit Page</h1>
+                        <h3>Product Information</h3>
+                        {/* use method override to change the method to PUT instead of POST */}
+                        <form action={`/products/${_id}?_method=PUT`} method='POST'>
+                            <fieldset>
+                                <label for="name">Name</label>
+                                <input class="form-input" type='text' name='name' defaultValue={name} />
+                                <label for="img">Image URL</label>
+                                <input class="form-input" type='text' name='img' defaultValue={img} />
+                                <label for="description">Description</label>
+                                <input class="form-input" type='text' name='description' defaultValue={description} />
+                                <label for="qty">Quantity</label>
+                                <input class="form-input" type='number' name='qty' defaultValue={qty} />
+                                <label for="price">Price</label>
+                                <input class="form-input" type='number' name='price' defaultValue={price} />
+                            </fieldset>
+                            <input class="form-btn btn-lg btn-block btn btn-dark" type='submit' name='' value='Submit Changes' />
+                        </form>
+                    </div>
                 </div>
             </Default>
         );
