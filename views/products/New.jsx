d29faa03c949497227ca1df0c5f815f03e42cd0c
Maha Mahmood <>
2020-08-24 22:48:38 -0400

    styled new product page

--- views/products/New.jsx
+++ views/products/New.jsx
@@ -5,16 +5,26 @@ class New extends React.Component {
     render() {
         return (
             <Default>
-                <div>
-                    <h1>New Product Page</h1>
-                    <form action='/products' method='POST'>
-                        Name: <input type='text' name='name' /><br />
-                        Image URL: <input type='text' name='img' /><br />
-                        Description: <input type='text' name='description' /><br />
-                        Quantity: <input type='number' name='qty' /><br />
-                        Price: <input type='number' name='price' /><br />
-                        <input type='submit' name='' value='Create New Product' />
-                    </form>
+                <div class="form-container">
+                    <div class="form">
+                        <h1 class="h1-form">New Product Page</h1>
+                        <h3>Product Information</h3>
+                        <form action='/products' method='POST'>
+                            <fieldset>
+                                <label for="name">Name</label> 
+                                <input class="form-input" type='text' name='name' /><br />
+                                <label for="img">Image URL</label>
+                                <input class="form-input"  type='text' name='img' /><br />
+                                <label for="description">Description</label> 
+                                <input class="form-input"  type='text' name='description' /><br />
+                                <label for="qty">Quantity</label>
+                                <input class="form-input"  type='number' name='qty' /><br />
+                                <label for="price">Price</label>
+                                <input class="form-input"  type='number' name='price' /><br />
+                            </fieldset>
+                            <input class="form-btn btn-lg btn-block btn btn-dark" type='submit' name='' value='Create New Product' />
+                        </form>
+                    </div>
                 </div>
             </Default>
         );
