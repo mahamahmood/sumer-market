a2e4634749c52164b3c04af627c906c6af0a27bf
Maha Mahmood <>
2020-08-25 21:08:38 -0400

    styled the bottons on the prodcut and recipes index pages

--- views/products/Index.jsx
+++ views/products/Index.jsx
@@ -13,7 +13,7 @@ class Index extends React.Component {
                         <h1 class='sumer-mine-h1'>Sumer Products</h1>
                         <div class="sumer-p-btn-create">
                             <p>Sweet beans affogato, sweet, instant aroma sit in flavour. Chicory, coffee pumpkin spice latte espresso, frappuccino single origin crema trifecta percolator coffee strong. Americano ut cappuccino as, qui, crema cultivar cappuccino affogato black robust.</p>
-                            <a class="btn btn-outline-dark" role="button" href="/products/new">Create a New Product Page</a>
+                            <a class="btn btn-dark" role="button" href="/products/new">Create a New Product Page</a>
                         </div>
                     </header>
                     <div class="container">
@@ -28,12 +28,15 @@ class Index extends React.Component {
                                                 <div class="card-body">
                                                     <a href={`/products/${product._id}`}><img class="img-fluid card-img" src={product.img} /></a>
                                                     <li class="card-title">Price: ${product.price}</li>
-                                                    <a class="btn btn-outline-dark btn-product" role="button" href={`/products/${product._id}`}>View</a>
-                                                    <a class="btn btn-outline-dark btn-product" role="button" href={`/products/${product._id}/edit`}>Edit</a>
+                                                    {/* buttons */}
+                                                    <div class="row">
+                                                    <a class="col btn btn-outline-dark btn-product" role="button" href={`/products/${product._id}`}>View</a>
+                                                    <a class="col btn btn-outline-dark btn-product" role="button" href={`/products/${product._id}/edit`}>Edit</a>
                                                     {/* override post method to delete the product of this uniq id */}
-                                                    <form action={`/products/${product._id}?_method=DELETE`} method='POST'>
-                                                        <input class="btn btn-outline-dark btn-product" type='submit' value='Delete' />
+                                                    <form class="col" action={`/products/${product._id}?_method=DELETE`} method='POST'>
+                                                        <input class="btn-delete btn btn-outline-dark btn-product" type='submit' value='Delete' />
                                                     </form>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
--- views/recipes/Index.jsx
+++ views/recipes/Index.jsx
@@ -9,11 +9,11 @@ class Index extends React.Component {
                 <div>
                     <header>
                         <img class="sumer-banner img-fluid" src="https://i.imgur.com/S5awAsP.jpg" />
-                            <h1 class='sumer-mine-h1'>Sumer Recipes <small><i>Shared by Our Community</i></small></h1>
-                            <div class="sumer-p-btn-create">
-                                <p>Our customers not only shop at Sumer Market but also they come here to share their best tips and tricks of making the best middel eastern dishes. Here you will be able to view all of the recipes that have been shared and <a href="/recipes/new">create your own recipe page.</a></p>
-                                <a class="btn btn-outline-dark" role="button" href="/recipes/new">Create a New Recipe Page</a>
-                            </div>
+                        <h1 class='sumer-mine-h1'>Sumer Recipes <small><i>Shared by Our Community</i></small></h1>
+                        <div class="sumer-p-btn-create">
+                            <p>Our customers not only shop at Sumer Market but also they come here to share their best tips and tricks of making the best middel eastern dishes. Here you will be able to view all of the recipes that have been shared and <a href="/recipes/new">create your own recipe page.</a></p>
+                            <a class="btn btn-dark" role="button" href="/recipes/new">Create a New Recipe Page</a>
+                        </div>
                     </header>
                     <div class="container">
                         <ul class="row row-cols-1 row-cols-md-2">
@@ -25,11 +25,14 @@ class Index extends React.Component {
                                                 <a href={`/recipes/${recipe._id}`}><img class="img-fluid card-img-top" src={recipe.img} /></a>
                                                 <div class="card-body">
                                                     <li class="card-title">{recipe.title}</li>
-                                                    <a class="btn btn-outline-dark btn-recipe" role="button" href={`/recipes/${recipe._id}`}>View</a>
-                                                    <a class="btn btn-outline-dark btn-recipe" role="button" href={`/recipes/${recipe._id}/edit`}>Edit</a>
-                                                    <form action={`/recipes/${recipe._id}?_method=DELETE`} method='POST'>
-                                                        <input class="btn btn-outline-dark btn-recipe" type='submit' value='Delete' />
-                                                    </form>
+                                                    {/* buttons */}
+                                                    <div class="row">
+                                                        <a class="col btn btn-outline-dark btn-recipe" role="button" href={`/recipes/${recipe._id}`}>View</a>
+                                                        <a class="col btn btn-outline-dark btn-recipe" role="button" href={`/recipes/${recipe._id}/edit`}>Edit</a>
+                                                        <form class="col" action={`/recipes/${recipe._id}?_method=DELETE`} method='POST'>
+                                                            <input class="btn-delete  btn btn-outline-dark btn-recipe" type='submit' value='Delete' />
+                                                        </form>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
